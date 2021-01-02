2fa04969bdb7b3cb5c4fbc833b48ee227011752c
zachl17 <zachluedtke17@gmail.com>
2021-01-02 12:54:26 -0600

    add js for portfolio page's pic slide

---
+++ LoadHeaderFooter.js
@@ -0,0 +1,7 @@
+
+        $(document).ready(function () {
+            $(function () {
+                $("#header").load("Header.html");
+                $("#footer").load("Footer.html");
+            });
+        });
