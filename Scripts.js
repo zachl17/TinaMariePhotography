054cac5d4a7a07fc8292c7383d61da086e009e39
zachl17 <zachluedtke17@gmail.com>
2021-01-11 04:35:45 -0600

    add fotorama css

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,5 @@
-var slideIndex = 1;
-var currentPath = $(location).attr('pathname');
+//var slideIndex = 1;
+//var currentPath = $(location).attr('pathname');
 
 $(function () {
     $("#header").load("Header.html");
@@ -7,32 +7,32 @@ $(function () {
     $("#footer").load("Footer.html");
 });
 
-if (currentPath == '/Portfolio.html') {
-    document.addEventListener("DOMContentLoaded", function () {
-        showSlides(slideIndex);
-    });
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides =  document.getElementsByClassName("mySlides");
-        var dots =  document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex - 1].style.display = "block";
-    }
-}
+//if (currentPath == '/Portfolio.html') {
+//    document.addEventListener("DOMContentLoaded", function () {
+//        showSlides(slideIndex);
+//    });
+//
+//    function plusSlides(n) {
+//        showSlides(slideIndex += n);
+//    }
+//
+//    function currentSlide(n) {
+//        showSlides(slideIndex = n);
+//    }
+//
+//    function showSlides(n) {
+//        var i;
+//        var slides =  document.getElementsByClassName("mySlides");
+//        var dots =  document.getElementsByClassName("dot");
+//        if (n > slides.length) {
+//            slideIndex = 1
+//        }
+//        if (n < 1) {
+//            slideIndex = slides.length
+//        }
+//        for (i = 0; i < slides.length; i++) {
+//            slides[i].style.display = "none";
+//        }
+//        slides[slideIndex - 1].style.display = "block";
+//    }
+//}
